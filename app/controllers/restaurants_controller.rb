c2d1ec286b4314e77720f774c259d1a90c69e859
luruski <luruski@rrc.ca>
2026-07-09 19:05:12 -0500

    restaurant index and show pages with bootstrap layout

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,9 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.order(:name)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
