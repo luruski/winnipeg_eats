af81c76a6a124e0f1cff2bac266f419209076ef1
luruski <luruski@rrc.ca>
2026-07-09 19:23:13 -0500

    search form with text search and cuisine dropdown filter

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,16 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.order(:name)
+
+    # simple text search on the name (feature 4.1)
+    if params[:search].present?
+      @restaurants = @restaurants.where("name LIKE ?", "%#{params[:search]}%")
+    end
+
+    # narrow the search down to one cuisine (feature 4.2)
+    if params[:cuisine_id].present?
+      @restaurants = @restaurants.joins(:cuisines).where(cuisines: { id: params[:cuisine_id] })
+    end
   end
 
   def show
